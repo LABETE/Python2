0c1390e8794e2c99dc5ec68e74a6e04625fa8b29
Eddie <eddiemaiden_17@hotmail.com>
2015-05-26 22:58:16 -0500

    Python 2 Projects

---
+++ Test/src/Test.py
@@ -0,0 +1,13 @@
+import unittest
+
+class TestMeNow(unittest.TestCase):
+    
+    def test_important_test(self):
+
+        print("If this doesn't run, I'm in big trouble")
+        self.assertNotEqual(2 + 2, 4, "They're the same!")
+
+if __name__ == "__main__":
+    unittest.main()
+
+   
